a73a1e441d53bf482ed3b1a4a7c26ffa13235c02
monde38 <rolando.mandefro@alumnos.upm.es>
2021-03-29 03:32:57 +0200

    first commit

---
+++ src/esp_wifi_station_module.h
@@ -0,0 +1,59 @@
+#ifndef ESP_WIFI_STATION_MODULE_H
+#define ESP_WIFI_STATION_MODULE_H
+
+#include "esp_sta.h"
+#include "esp_wifi.h"
+#include <string.h>
+
+
+typedef struct esp_wifi_station_module{
+
+    struct station_config esp;
+}esp_wifi_station_module;
+
+
+
+/**
+  * @brief  Initialize WiFi connection betwen a ESP8266 in station mode and an AP. Make sure initialize a WiFi Station Module first.
+  *
+  * @param     esp_wifi_station_module* mod       : station module.
+  * @param     char*  ssid                        : AP name.
+  * @param     char*  password                    : AP password name.
+  *
+  * @return    void             
+  */
+void init_module_station(esp_wifi_station_module* mod, char* ssid, char* password){
+
+    wifi_set_opmode_current(STATION_MODE);
+    strcpy((char*)(mod->esp.ssid), ssid);
+    strcpy((char*)(mod->esp.password), password);
+    wifi_station_set_config_current(&(mod->esp));
+    wifi_station_connect();
+}
+
+
+/**
+  * @brief  Check if connection is ok.
+  *
+  * @param     void
+  *
+  * @return    true  : connected
+  * @return    false : not connected 
+  */
+bool station_connected(void) {if (wifi_station_get_connect_status() == STATION_GOT_IP) return 1; else return 0;}
+
+
+
+/**
+  * @brief  Check if operation modeis correct.
+  *
+  * @param     void
+  *
+  * @return    true  : correct
+  * @return    false : not correct 
+  */
+bool check_station_operation_mode(void) {if(wifi_get_opmode_default() == STATION_MODE) return 1; else return 0;}
+
+
+
+#endif 
---
+++ src/main.c
@@ -0,0 +1,75 @@
+#include "esp_common.h"
+#include "freertos/task.h"
+#include "gpio.h"
+#include "esp_wifi_station_module.h"
+
+#define ap_name        "monde38"
+#define ap_password    "monde.38"
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+uint32 user_rf_cal_sector_set(void)
+{
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void task_blink(void* ignore)
+{   
+
+    esp_wifi_station_module mod;
+    init_module_station( &mod, ap_name, ap_password);
+
+    while(true) {}
+
+
+    vTaskDelete(NULL);
+}
+
+
+
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void user_init(void)
+{
+    xTaskCreate(&task_blink, "startup", 2048, NULL, 1, NULL);
+}
